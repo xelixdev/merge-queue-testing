9091fc99ad2dfc8976ad165f695ed90b04b27566
Frank-Howard <frank.howard@xelix.com>
2024-11-04 17:53:31 +0000

    test merge queue script

---
+++ test_merge_queue.sh
@@ -0,0 +1,10 @@
+git checkout main
+for i in {1..10};
+do
+	git checkout -b merge-queue-test$i
+	touch mq-test$i
+	git add mq-test$i
+	git commit -m "mq-test$i"
+	gh pr create --body="" --title=""
+	git checkout main
+done	
